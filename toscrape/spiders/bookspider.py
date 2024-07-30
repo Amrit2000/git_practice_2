3474d761da0f3c55fbd2a6c27cd8902bf7675056
Amrit2000 <amrit898singh@gmail.com>
2024-07-30 16:16:24 +0530

    scraping general 1st

--- toscrape/spiders/bookspider.py
+++ toscrape/spiders/bookspider.py
@@ -1,10 +1,44 @@
+# import scrapy
+
+
+# class BookspiderSpider(scrapy.Spider):
+#     name = "bookspider"
+#     allowed_domains = ["quotes.toscrape.com"]
+#     start_urls = ["https://quotes.toscrape.com/"]
+
+#     def parse(self, response):
+#         # total = response.xpath('//div[@class="col-md-8"]/text()')
+
+#         quote = response.xpath('//span[@class="text"]/text()').getall()
+#         author = response.xpath('//small[@class="author"]/text()').getall()
+
+#         for i in range(len(quote)):
+#             yield quote[i]
+
+#         # pass
+
+
 import scrapy
 
-
-class BookspiderSpider(scrapy.Spider):
+class QuotesSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        quotes = response.xpath('//div[@class="quote"]')
+        
+        for quote in quotes:
+            text = quote.xpath('span[@class="text"]/text()').get()
+            author = quote.xpath('span/small[@class="author"]/text()').get()
+            tags = quote.xpath('div[@class="tags"]/a[@class="tag"]/text()').getall()
+            
+            yield {
+                'quote': text,
+                'author': author,
+                'tags': tags
+            }
+
+        next_page = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
